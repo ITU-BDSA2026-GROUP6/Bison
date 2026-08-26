007e02419406c855b7886dd4db38cf3962873319
Axel Jensen <axje@itu.dk>
2026-08-26 14:10:20 +0200

    Read observations from CSV file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿public class Observation
+{
+    public string Author { get; set; } = string.Empty;
+    public string ObservationText { get; set; } = string.Empty;
+    public string Timestamp { get; set; } = string.Empty;
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string filePath = Path.Combine("CSVfiles", "bison_observe_cli_db.csv");
+            using StreamReader reader = new(filePath);
+            string text = reader.ReadToEnd();
+
+            Console.WriteLine(text);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Couldn't read file: {e.Message}"); 
+        }
+    }
     
-//Hallo
+}
