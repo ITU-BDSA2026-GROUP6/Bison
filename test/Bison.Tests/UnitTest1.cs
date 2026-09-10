6918c71b391211aff2643052df3ebde5a26a1e96
Bertram_Jensen <behj@itu.dk>
2026-09-10 12:42:39 +0200

    add solution and test projects

---
+++ test/Bison.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Bison.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/CSVDatabase.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CSVDatabase.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
