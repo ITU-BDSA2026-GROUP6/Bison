566d9ff80054312fad0546c18711633df72ac87d
Vitusjh <Va.holck@gmail.com>
2026-09-14 20:16:38 +0200

    g to G

--- test/CSVDatabase.Tests/Integrationstests1.cs
+++ test/CSVDatabase.Tests/Integrationstests1.cs
@@ -8,7 +8,7 @@ public class IntegrationTests
     {
         // Arrange
         string temporaryFilePath = Path.Combine(Path.GetTempPath(), $"test_bison_observe_cli_db_{Guid.NewGuid()}.csv");
-        var database = CSVDatabase<Observation>.getInstance(temporaryFilePath);
+        var database = CSVDatabase<Observation>.GetInstance(temporaryFilePath);
 
         Observation observationToStore = new Observation(1, "test_author", "test_observation", "Test Location", 1700000000);
 
@@ -32,7 +32,7 @@ public class IntegrationTests
     {
         // Arrange
         string temporaryFilePath = Path.Combine(Path.GetTempPath(), $"test_bison_observe_cli_db_{Guid.NewGuid()}.csv");
-        var database = CSVDatabase<Observation>.getInstance(temporaryFilePath);
+        var database = CSVDatabase<Observation>.GetInstance(temporaryFilePath);
 
         Observation observation1 = new Observation(1, "author1", "observation1", "Test Location 1", 1700000000);
         Observation observation2 = new Observation(2, "author2", "observation2", "Test Location 2", 1700000001);
