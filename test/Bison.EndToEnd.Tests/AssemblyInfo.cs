be91b5ace556f8ffe0f7e7a17da137dee06f27cb
Bertram_Jensen <behj@itu.dk>
2026-09-24 13:46:59 +0200

    Moved all relevant bisonCLI contents to seperate branch bison_cli
    Main branch now contains boilerplate code from course template, and is ready to be rebuilt as a razor application.

--- test/Bison.EndToEnd.Tests/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using Xunit;
-
-// E2E tests each spin up a real web service process that shares the same
-// on-disk CSV storage , so running test classes in parallel causes cross-test data failure.
-
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
